d906ff041232cd657272720166f6d2253d5e6343
hw2752 <hw2752@columbia.edu>
2019-12-07 17:36:11 +0000

    change the path of import.py

--- st/management/commands/import_squirrel_data.py
+++ st/management/commands/import_squirrel_data.py
@@ -1,12 +1,9 @@
 import csv
 from django.core.management.base import BaseCommand
-
 class Command(BaseCommand):
     help = 'Import the data of squirrel'
-
     def add_arguments(self,parser):
         parser.add_argument('path',type=str)
-
     def handle(self,*args,**options):
         with open(options['path']) as sd:
             data = csv.DictReader(sd)
